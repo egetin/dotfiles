2ea84a7fc4989ae0b8e9e62bf3672c46c31f9325
Erkka Mäkinen <erkka.makinen@gmail.com>
2017-01-29 12:40:03 +0200

    Add latest fish conf

--- t420/.config/fish/config.fish
+++ t420/.config/fish/config.fish
@@ -7,3 +7,10 @@ set -x GOPATH "/home/erkka/Go"
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 
 set -x VAGRANT_DEFAULT_PROVIDER virtualbox
+
+# wrapper for nvm command
+function nvm
+  bass source ~/.nvm/nvm.sh ';' nvm $argv
+end
+
+bass source ~/.nvm/nvm.sh ';' nvm >> /dev/null
