71b7098c7100b809dfbb4cb0fd93d1c407cb8204
Erkka Mäkinen <erkka.makinen@hillcode.fi>
2021-04-19 08:31:11 +0300

    thefuck configuration to fish config

--- x1carbon/.config/fish/config.fish
+++ x1carbon/.config/fish/config.fish
@@ -3,8 +3,8 @@
 set -x VISUAL vim
 set -x EDITOR $VISUAL
 
-set -x GOPATH "/home/emak/Go"
-set -x PATH $PATH /usr/lib/go/bin $GOPATH/bin /home/emak/.cargo/bin /home/emak/.bin /home/emak/Random/kubernetes/kubernetes/client/bin
+set -x GOPATH "/home/erkka/Go"
+set -x PATH $PATH /usr/lib/go/bin $GOPATH/bin $HOME/.cargo/bin $HOME/.bin $HOME/.local/bin $HOME/.local/share/flutter/bin $HOME/.local/share/android-studio/bin
 
 set -x VAGRANT_DEFAULT_PROVIDER virtualbox
 
@@ -17,17 +17,6 @@ function nvm
 end
 
 bass source ~/.nvm/nvm.sh ';' nvm >> /dev/null
-#function fuck -d "Correct your previous console command"
-#  set -l fucked_up_command $history[1]
-#  env TF_ALIAS=fuck PYTHONIOENCODING=utf-8 thefuck $fucked_up_command | read -l unfucked_command
-#  if [ "$unfucked_command" != "" ]
-#    eval $unfucked_command
-#    builtin history delete --exact --case-sensitive -- $fucked_up_command
-#    builtin history merge ^ /dev/null
-#  end
-#end
-#
-#thefuck --alias | source
 
 function max-brightness
   xbacklight -set 100%
@@ -40,3 +29,5 @@ end
 function set-brightness
   xbacklight -set $argv
 end
+
+thefuck --alias | source
